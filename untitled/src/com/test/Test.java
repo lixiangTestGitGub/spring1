1cfd2e6e2164d9fb88c8103e917a5387d8d82bd3
swwlz <373794826@qq.com>
2022-02-28 20:20:44 +0800

    commit message

---
+++ untitled/src/com/test/Test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * @ClassName Test
+ * @Description 测试github
+ * @Author LiXiang
+ * @Group alien
+ * @Date 2022/2/28 20:13
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("lx 的测试");
+    }
+}
